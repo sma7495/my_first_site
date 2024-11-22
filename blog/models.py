564aa53cad8a6c489ec94344e5e5586606def2ae
sma7495 <aut.aseyedali@gmail.com>
2024-11-22 17:10:23 +0330

    1. the diagram of the project data base was created in SQL Draw 2. The tables "Post" and "Contact" were created 3. admin.py for Post model was registered

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class Post(models.Model):
+    #image
+    #author
+    title = models.CharField(max_length=150)
+    content = models.TextField()
+    #tag
+    #category
+    counted_view = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(default= timezone.now())
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.title
+
+    
