0d0353272499744b70e8156eabdc4c01fbf6ca90
sma7495 <aut.aseyedali@gmail.com>
2024-12-20 16:35:21 +0330

    Pagination was added to blog

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from blog import models
 from django.db.models import Q
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 
 def index_view(request):
     posts = models.Post.objects.filter(status = 1).order_by("-published_date")
+    p = Paginator(posts,3)
+    try:
+        posts = p.page(request.GET.get("page"))
+    except PageNotAnInteger:
+        posts = p.page(1)
+    except EmptyPage:
+        posts = p.page(1)
+        
+
+
     contex = {'posts': posts}
 
     return render( request,'blog/blog-home.html', contex)
@@ -23,6 +34,15 @@ def category_view(request, cat=None, writer=None):
 
     if writer:
         posts = models.Post.objects.filter(status = 1, author__first_name = writer).order_by("-published_date")
+
+    p = Paginator(posts,3)
+    try:
+        posts = p.page(request.GET.get("page"))
+    except PageNotAnInteger:
+        posts = p.page(1)
+    except EmptyPage:
+        posts = p.page(1)
+
     contex = {'posts': posts}
     return render( request,'blog/blog-home.html', contex)   
 
@@ -30,7 +50,15 @@ def search_view(request):
     if request.method == "GET":
         if s:=request.GET.get("s"):
             posts = models.Post.objects.filter(status = 1).filter(Q(title__icontains = s) | Q(content__icontains = s))
-            contex = {'posts': posts}
+            p = Paginator(posts,3)
+            try:
+                posts = p.page(request.GET.get("page"))
+            except PageNotAnInteger:
+                posts = p.page(1)
+            except EmptyPage:
+                posts = p.page(1)
+                
+            contex = {'posts': posts, 'search':s}
             return render( request,'blog/blog-home.html', contex)
         else:
             return redirect("website:index")
